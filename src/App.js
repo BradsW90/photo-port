6b56bae829aca3fc1fcbe8aa46cabbb5e00e0f75
Bradley <bswj90@gmail.com>
2022-09-07 22:59:53 -0400

    setup useEffect and useState to update page dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 import "./App.css";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    {
+      name: "protraits",
+      description: "Portraits of people in my life",
+    },
+    {
+      name: "food",
+      descrition: "Delicious delicacies",
+    },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div className="App">
       <header className="App-header">
-        <Nav></Nav>
+        <Nav
+          categories={categories}
+          setCurrentCategory={setCurrentCategory}
+          currentCategory={currentCategory}
+        ></Nav>
         <main>
+          <Gallery></Gallery>
           <About></About>
         </main>
       </header>
